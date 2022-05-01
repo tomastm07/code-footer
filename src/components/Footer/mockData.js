53a4884aaef475cea053b065b0ff7acdd20efb3b
marin73tomas <marin73toma@gmail.com>
2022-05-01 13:19:44 -0400

    added tailwind

---
+++ src/components/Footer/mockData.js
@@ -0,0 +1,298 @@
+const desktopFooter = {
+     legalText: [
+       {
+         children: [
+           {
+             text:
+               "All Rights Reserved | Made in NYC | Terms & Conditions | Privacy Policy"
+           }
+         ],
+         type: "paragraph"
+       }
+     ],
+     name: "Desktop",
+     navLinkGroups: [
+       {
+         _id: "f9858fdb-c355-47ea-bf14-8763e28d4ea3",
+         displayText: "Shop",
+         name: "Shop ",
+         navLinks: [
+           {
+             _id: "83fec4a0-f63c-4ff9-9474-d8c8b6b443a0",
+             displayText: "Shop All",
+             name: "Shop All",
+             url: "https://www.fellowproducts.com/collections/all"
+           },
+           {
+             _id: "53e30787-f136-44cb-8cad-7fef0cf0f58b",
+             displayText: "Best Sellers",
+             name: "Best Sellers",
+             url: "https://www.fellowproducts.com/collections/best-sellers"
+           },
+           {
+             _id: "7a2a5185-b4ff-4c80-9617-d3436afd3cf2",
+             displayText: "Value Kits",
+             name: "Value Kits",
+             url: "https://www.fellowproducts.com/collections/kits-gifts"
+           },
+           {
+             _id: "2555cfd3-d00f-4183-807a-0fe94e507bb1",
+             displayText: "Skin Quiz",
+             name: "Skin Quiz",
+             url: "https://www.fellowproducts.com/pages/skincare-finder"
+           },
+           {
+             _id: "5e876984-50eb-4cec-a198-0736d1de2f23",
+             displayText: "Store Locator",
+             name: "Store Locator",
+             url: "https://www.fellowproducts.com/pages/store-locator"
+           }
+         ],
+         url: ""
+       },
+       {
+         _id: "5427adda-0723-4430-a013-803b27431785",
+         displayText: "Learn More",
+         name: "Learn More",
+         navLinks: [
+           {
+             _id: "bbd6a986-c30f-4f8f-bf77-5daa256d251f",
+             displayText: "Probiotics & Superfoods",
+             name: "Probiotics & Superfoods",
+             url: "https://www.fellowproducts.com/pages/probiotic-skincare"
+           },
+           {
+             _id: "0f599abb-7402-43f2-b961-bf856e1282e0",
+             displayText: "Refer Friends",
+             name: "Refer Friends",
+             url: "https://www.fellowproducts.com/pages/share"
+           },
+           {
+             _id: "5f5b0b47-379f-4f0e-8943-b41bc0bae487",
+             displayText: "Press",
+             name: "Press",
+             url: "https://www.fellowproducts.com/blogs/press"
+           },
+           {
+             _id: "8854da0f-f25e-4951-9e1a-57fa0dc04f49",
+             displayText: "Video Testimonials",
+             name: "Video Testimonials",
+             url: "https://www.fellowproducts.com/pages/video-testimonials"
+           }
+         ],
+         url: ""
+       },
+       {
+         _id: "eb3e8957-f7b0-4ad0-941d-c06682d55834",
+         displayText: "About",
+         name: "About",
+         navLinks: [
+           {
+             _id: "3d0bd311-df40-43da-95d8-44d6fadfe1a4",
+             displayText: "Clean & Effective",
+             name: "Clean & Effective",
+             url: "https://www.fellowproducts.com/pages/clean-effective"
+           },
+           {
+             _id: "579e67b4-5bfc-4efc-ad45-7ab26c67cd48",
+             displayText: "Our Story & Founder",
+             name: "Our Story & Founder",
+             url: "https://www.fellowproducts.com/pages/dr-raj"
+           },
+           {
+             _id: "614766fd-5718-452c-8aa2-25fc87057671",
+             displayText: "Careers",
+             name: "Careers",
+             url: "https://boards.greenhouse.io/fellowproducts"
+           },
+           {
+             _id: "4bcd165b-27a0-4e5f-befb-e123969c3daf",
+             displayText: "Ambassador Program",
+             name: "Ambassador Program",
+             url:
+               "https://www.fellowproducts.com/pages/fellowproducts-influencer-ambassador-program"
+           },
+           {
+             _id: "082f175b-080b-4bcf-acd9-c54894ce2061",
+             displayText: "Giving Back",
+             name: "Giving Back",
+             url:
+               "https://www.fellowproducts.com/pages/fellowproducts-medical-school-scholarship-program"
+           }
+         ],
+         url: ""
+       },
+       {
+         _id: "1c954af0-527a-49b3-a140-22f4e3f1250f",
+         displayText: "Help",
+         name: "Help",
+         navLinks: [
+           {
+             _id: "7babf452-6ac5-4be0-9b9c-5ee59f935bb7",
+             displayText: "FAQs",
+             name: "FAQs",
+             url: "https://www.fellowproducts.com/pages/faq"
+           },
+           {
+             _id: "1b6c95ba-bfc9-41df-8b4c-5c2a163fa790",
+             displayText: "Returns & Exchanges",
+             name: "Returns & Exchanges",
+             url: "https://returns.fellowproducts.com/"
+           },
+           {
+             _id: "e70033e9-a78f-465a-98a7-3e8dfa64cde3",
+             displayText: "Contact Us",
+             name: "Contact Us",
+             url: "https://www.fellowproducts.com/pages/contact-us"
+           },
+           {
+             _id: "193bfd5b-9dbf-4ec7-8e54-27d43eb1c018",
+             displayText: "Accessibility",
+             name: "Accessibility",
+             url: "https://www.fellowproducts.com/pages/accessibility"
+           }
+         ],
+         url: ""
+       }
+     ]
+   };
+   
+   const mobileFooter = {
+     legalText: [
+       {
+         children: [
+           {
+             text: "All Rights Reserved | Made in NYC"
+           }
+         ],
+         type: "paragraph"
+       },
+       {
+         children: [
+           {
+             text: "Terms & Conditions | Privacy Policy"
+           }
+         ],
+         type: "paragraph"
+       }
+     ],
+     name: "Mobile",
+     navLinkGroups: [
+       {
+         _id: "7007fe05-bbfd-4442-9cfa-c3d7f74eb717",
+         displayText: "faq",
+         url: "https://www.fellowproducts.com/pages/faq"
+       },
+       {
+         _id: "0b84c314-6692-42d0-ab1b-d053ff4b76d5",
+         displayText: "returns & exchanges",
+         url: "https://returns.fellowproducts.com/"
+       },
+       {
+         _id: "3c36c98d-383f-4150-a14b-804ca3b19cb0",
+         displayText: "contact us",
+         url: "https://www.fellowproducts.com/pages/contact-us"
+       },
+       {
+         _id: "83b14c16-b9f2-483a-864b-154c3cdee8e5",
+         displayText: "accessibility",
+         url: "https://www.fellowproducts.com/pages/accessibility"
+       },
+       {
+         _id: "f9858fdb-c355-47ea-bf14-8763e28d4ea3",
+         displayText: "Shop",
+         navLinks: [
+           {
+             _id: "83fec4a0-f63c-4ff9-9474-d8c8b6b443a0",
+             displayText: "Shop All",
+             url: "https://www.fellowproducts.com/collections/all"
+           },
+           {
+             _id: "53e30787-f136-44cb-8cad-7fef0cf0f58b",
+             displayText: "Best Sellers",
+             url: "https://www.fellowproducts.com/collections/best-sellers"
+           },
+           {
+             _id: "7a2a5185-b4ff-4c80-9617-d3436afd3cf2",
+             displayText: "Value Kits",
+             url: "https://www.fellowproducts.com/collections/kits-gifts"
+           },
+           {
+             _id: "2555cfd3-d00f-4183-807a-0fe94e507bb1",
+             displayText: "Skin Quiz",
+             url: "https://www.fellowproducts.com/pages/skincare-finder"
+           },
+           {
+             _id: "5e876984-50eb-4cec-a198-0736d1de2f23",
+             displayText: "Store Locator",
+             url: "https://www.fellowproducts.com/pages/store-locator"
+           }
+         ],
+         url: ""
+       },
+       {
+         _id: "5427adda-0723-4430-a013-803b27431785",
+         displayText: "Learn More",
+         navLinks: [
+           {
+             _id: "bbd6a986-c30f-4f8f-bf77-5daa256d251f",
+             displayText: "Probiotics & Superfoods",
+             url: "https://www.fellowproducts.com/pages/probiotic-skincare"
+           },
+           {
+             _id: "0f599abb-7402-43f2-b961-bf856e1282e0",
+             displayText: "Refer Friends",
+             url: "https://www.fellowproducts.com/pages/share"
+           },
+           {
+             _id: "5f5b0b47-379f-4f0e-8943-b41bc0bae487",
+             displayText: "Press",
+             url: "https://www.fellowproducts.com/blogs/press"
+           },
+           {
+             _id: "8854da0f-f25e-4951-9e1a-57fa0dc04f49",
+             displayText: "Video Testimonials",
+             url: "https://www.fellowproducts.com/pages/video-testimonials"
+           }
+         ],
+         url: ""
+       },
+       {
+         _id: "eb3e8957-f7b0-4ad0-941d-c06682d55834",
+         displayText: "About",
+         navLinks: [
+           {
+             _id: "3d0bd311-df40-43da-95d8-44d6fadfe1a4",
+             displayText: "Clean & Effective",
+             url: "https://www.fellowproducts.com/pages/clean-effective"
+           },
+           {
+             _id: "579e67b4-5bfc-4efc-ad45-7ab26c67cd48",
+             displayText: "Our Story & Founder",
+             url: "https://www.fellowproducts.com/pages/dr-raj"
+           },
+           {
+             _id: "614766fd-5718-452c-8aa2-25fc87057671",
+             displayText: "Careers",
+             url: "https://boards.greenhouse.io/fellowproducts"
+           },
+           {
+             _id: "4bcd165b-27a0-4e5f-befb-e123969c3daf",
+             displayText: "Ambassador Program",
+             url:
+               "https://www.fellowproducts.com/pages/fellowproducts-influencer-ambassador-program"
+           },
+           {
+             _id: "082f175b-080b-4bcf-acd9-c54894ce2061",
+             displayText: "Giving Back",
+             url:
+               "https://www.fellowproducts.com/pages/fellowproducts-medical-school-scholarship-program"
+           }
+         ],
+         url: ""
+       }
+     ]
+   };
+   
+   export { desktopFooter, mobileFooter };
+   
--- src/index.js
+++ src/index.js
@@ -1,13 +1,12 @@
 import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
+import ReactDOM from "react-dom";
 
 import App from "./App";
 
 const rootElement = document.getElementById("root");
-const root = createRoot(rootElement);
-
-root.render(
+ReactDOM.render(
   <StrictMode>
     <App />
-  </StrictMode>
+  </StrictMode>,
+  rootElement
 );
