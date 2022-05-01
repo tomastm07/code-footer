09239ae7b6f36e32b6232784557e767975ab6b40
marin73tomas <marin73toma@gmail.com>
2022-05-01 13:48:01 -0400

    working footer

---
+++ src/components/Footer/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import mockData from "./mockData"
+
+const Footer = ({ desktopFooter, mobileFooter }) => {
+  console.log(mobileFooter);
+  console.log(desktopFooter);
+  return <footer></footer>;
+};
+
+export default Footer;
